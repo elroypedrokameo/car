45da4ae13110d74266ee9f8b67a90c3520bba9a2
Elroy Pedro Kameo <infinitygamexray@gmail.com>
2021-05-16 11:38:56 +0800

    Main Page

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,21 +1,23 @@
 import React from 'react'
 import styled from "styled-components"
 
-function Section() {
+function Section({ title, description, leftBtnText, rightBtnText, backgroundImg }) {
     return (
-        <Wrap>
+        <Wrap bgImage={backgroundImg}>
             <ItemText>
-                <h1>Model S</h1>
-                <p>Order Online for Touchless Delivery</p>
+                <h1>{ title }</h1>
+                <p>{ description }</p>
             </ItemText>
             <Buttons>
                 <ButtonGroup>
                     <LeftButton>
-                        Custom Order
+                        { leftBtnText }
                     </LeftButton>
-                    <RigthButton>
-                        Existing Inventory
-                    </RigthButton>
+                    { rightBtnText && 
+                        <RigthButton>
+                            { rightBtnText }
+                        </RigthButton>
+                    }
                 </ButtonGroup>
             <DownArrow src="/images/down-arrow.svg" />
             </Buttons>
@@ -36,6 +38,7 @@ const Wrap = styled.div`
     flex-direction: column;
     justify-content: space-between; //vertikal
     align-items: center; // horizontal
+    background-image: ${props => `url("/images/${props.bgImage}")`}
 `
 
 const ItemText = styled.div`
